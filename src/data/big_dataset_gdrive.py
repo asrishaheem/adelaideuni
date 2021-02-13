d4b2f3ab492b1b37e53dcf2a9bec7c202d6aa7e9
asrishaheem <asrishaheem@gmail.com>
2021-02-13 21:04:36 +1030

    script modify

--- src/data/big_dataset_gdrive.py
+++ src/data/big_dataset_gdrive.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def gdrive_download(FILE_ID, FILENAME):
-    return subprocess.call("./src/scripts/gdrive-download.sh", FILE_ID, FILENAME)
+    return subprocess.check_call(["./src/scripts/gdrive-download.sh", FILE_ID, FILENAME])
 
 def gdrive_unzip(FILENAME):
     return subprocess.call("./src/scripts/gdrive-unzip.sh", FILENAME)
